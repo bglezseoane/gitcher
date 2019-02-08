9bf7090ee62f0c8d5f594953de7eb48f305c0c4a
Borja González Seoane <dev@glezseoane.com>
2019-02-08 14:01:28 +0100

    feat: Create a not found profile case exception

---
+++ gitcher/not_found_prof_error.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+"""gitcher not_found_prof_error class module
+
+This module constains the class taht represents a gitcher not found profile
+exception.
+"""
+
+# Authorship
+__author__ = 'Borja González Seoane'
+__copyright__ = 'Copyright 2019, Borja González Seoane'
+__credits__ = 'Borja González Seoane'
+__license__ = 'LICENSE'
+__version__ = '0.1a1'
+__maintainer__ = 'Borja González Seoane'
+__email__ = 'dev@glezseoane.com'
+__status__ = 'Development'
+
+
+class NotFoundProfError(Exception):
+    """Class that represents a gitcher gitcher not found profile exception."""
+    pass
