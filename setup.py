c345cbf6fc218ba18211ad1a6f784a9a9b944601
Borja González Seoane <dev@glezseoane.com>
2019-07-01 13:16:18 +0200

    fix: Update setup configuration
    Redirects to GitHub server and new release version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ __status__ = 'Production'
 
 setup(
     name='gitcher',
-    version='2.1',
+    version='2.2',
     packages=['gitcher'],
     entry_points={
         'console_scripts': [
@@ -29,9 +29,8 @@ setup(
     install_requires=['validate_email==1.3', 'prettytable==0.7.2'],
     data_files=[('share/man/man1', ['manpages/gitcher.1']),
                 ("", ["LICENSE"])],
-    url='https://gitlab.com/GlezSeoane/gitcher',
-    download_url='https://gitlab.com/GlezSeoane/gitcher/-/archive/v2.1'
-                 '/gitcher-v2.1.tar.gz',
+    url='https://github.com/GlezSeoane/gitcher',
+    download_url='https://github.com/GlezSeoane/gitcher/archive/v2.2.tar.gz',
     license='LICENSE',
     author='Borja González Seoane',
     author_email='dev@glezseoane.com',
